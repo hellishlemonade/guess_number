ab012283fbb53c183a4c03f5e2a461e222665069
Dmitriy Lyapin <hellishlemonade111@gmail.com>
2024-09-24 17:46:12 +0300

    optimize key logic

--- guess_number.py
+++ guess_number.py
@@ -10,10 +10,10 @@ while True:
     if guess < number:
         print('Ваше число меньше того, что загадано.')
 
-    if guess > number:
+    elif guess > number:
         print('Ваше число больше того, что загадано.')
 
-    if guess == number:
+    elif guess == number:
         break
 
 
